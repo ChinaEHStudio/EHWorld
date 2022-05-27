7eb70014c6ac816f04a93938fe8846fa3366d8a6
xiaowanggua <2362301355@qq.com>
2022-05-27 20:16:07 +0800

    Create hash.cs

---
+++ Data/hash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EHWorld.Models
+{
+    public class hash {
+
+        private String key;
+        public hash(String key)
+        {
+            this.key = key;
+        }
+        public String ApplyHash(String word)
+        {
+            byte[] data = ASCIIEncoding.Unicode.GetBytes(word+key);
+            byte[] result;
+#pragma warning disable SYSLIB0021 // 类型或成员已过时
+            SHA512 shaM = new SHA512Managed();
+#pragma warning restore SYSLIB0021 // 类型或成员已过时
+            result = shaM.ComputeHash(data);
+            return ASCIIEncoding.Unicode.GetString(result);
+        }
+    }
+}
